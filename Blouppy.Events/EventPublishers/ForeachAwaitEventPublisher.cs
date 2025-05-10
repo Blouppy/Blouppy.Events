4cadbccc268f569cfad27967375d5e553a286047
Jimmy Boinembalome <jboinembalome@gmail.com>
2025-05-10 23:17:04 +0200

    Add XML documentation

--- Blouppy.Events/EventPublishers/ForeachAwaitEventPublisher.cs
+++ Blouppy.Events/EventPublishers/ForeachAwaitEventPublisher.cs
@@ -14,6 +14,12 @@ namespace Blouppy.Events.EventPublishers;
 public sealed class ForeachAwaitEventPublisher : IEventPublisher
 {
     /// <summary>
+    /// Publishes an event to all handlers using a foreach loop.
+    /// </summary>
+    /// <param name="handlerExecutors">The list of handler executors.</param>
+    /// <param name="event">The event to publish.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the publish operation.</returns>
     public async Task PublishAsync(
         IEnumerable<EventHandlerExecutor> handlerExecutors, 
         IEvent @event, 
--- Blouppy.Events/EventPublishers/IEventPublisher.cs
+++ Blouppy.Events/EventPublishers/IEventPublisher.cs
@@ -9,5 +9,11 @@ namespace Blouppy.Events.EventPublishers;
 public interface IEventPublisher
 {
     /// <summary>
+    /// Publishes an event to all handlers.
+    /// </summary>
+    /// <param name="handlerExecutors">The list of handler executors.</param>
+    /// <param name="event">The event to publish.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the publish operation.</returns>
     Task PublishAsync(IEnumerable<EventHandlerExecutor> handlerExecutors, IEvent @event, CancellationToken cancellationToken);
 }
